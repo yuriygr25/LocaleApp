bc53dbf43a9c041cb56f72fb9454a57f94ab5106
Yuriy Ivanov <yuriygr@yahoo.com>
2018-10-18 00:21:54 +0300

    commit

---
+++ app/src/main/java/com/example/yura/localeapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.yura.localeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
